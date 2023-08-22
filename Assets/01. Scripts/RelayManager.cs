0ab29eab16fcd3014f99885db9318e6030f05b68
cherry-kiwi <hmongsangga@gmail.com>
2023-08-22 09:37:18 +0900

    Done!

--- Assets/01. Scripts/RelayManager.cs
+++ Assets/01. Scripts/RelayManager.cs
@@ -1,18 +1,92 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Core.Environments;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class RelayManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public struct RelayHostData
     {
-        
+        public string joinCode;
+        public string IPv4Address;
+        public ushort port;
+        public Guid allocationID;
+        public byte[] allocationIDBytes;
+        public byte[] connectionData;
+        public byte[] key;
     }
 
-    // Update is called once per frame
-    void Update()
+    public struct RelayJoinData
     {
-        
+        public string IPv4Address;
+        public ushort port;
+        public Guid allocationID;
+        public byte[] allocationIDBytes;
+        public byte[] connectionData;
+        public byte[] hostConnectionData;
+        public byte[] key;
+    }
+
+    public static async Task<RelayHostData> SetupRelay(int maxConn, string environment)
+    {
+        InitializationOptions options = new InitializationOptions().SetEnvironmentName(environment);
+
+        await UnityServices.InitializeAsync(options);
+
+        if (!AuthenticationService.Instance.IsSignedIn) //로그인이 안되어있다면
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync(); //익명으로 시작하기
+        }
+
+        Allocation allocation = await Unity.Services.Relay.RelayService.Instance.CreateAllocationAsync(maxConn);
+
+        RelayHostData data = new RelayHostData
+        {
+            IPv4Address = allocation.RelayServer.IpV4,
+            port = (ushort)allocation.RelayServer.Port,
+
+            allocationID = allocation.AllocationId,
+            allocationIDBytes = allocation.AllocationIdBytes,
+            connectionData = allocation.ConnectionData,
+            key = allocation.Key,
+        };
+
+        data.joinCode = await Unity.Services.Relay.RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        return data;
+    }
+
+    public static async Task<RelayJoinData> JoinRelay(string joinCode, string environment)
+    {
+        Debug.LogError($"Start Join by{joinCode}");
+        InitializationOptions options = new InitializationOptions().SetEnvironmentName(environment);
+
+        await UnityServices.InitializeAsync(options);
+
+        if (!AuthenticationService.Instance.IsSignedIn) //로그인이 안되어있다면
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync(); //익명으로 시작하기
+        }
+
+        JoinAllocation allocation = await Unity.Services.Relay.RelayService.Instance.JoinAllocationAsync(joinCode);
+
+        RelayJoinData data = new RelayJoinData
+        {
+            IPv4Address = allocation.RelayServer.IpV4,
+            port = (ushort)allocation.RelayServer.Port,
+
+            allocationID = allocation.AllocationId,
+            allocationIDBytes = allocation.AllocationIdBytes,
+            connectionData = allocation.ConnectionData,
+            hostConnectionData = allocation.HostConnectionData,
+            key = allocation.Key,
+        };
+
+        return data;   
     }
 }
--- Assets/01. Scripts/UIManager.cs
+++ Assets/01. Scripts/UIManager.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Unity.Netcode;
+using TMPro;
+using Unity.Netcode.Transports.UTP;
 
 public class UIManager : MonoBehaviour
 {
     public Button hostButton;
     public Button clientButton;
+    public TMP_InputField inputField;
 
     private void Start()
     {
@@ -15,13 +18,26 @@ public class UIManager : MonoBehaviour
         clientButton.onClick.AddListener(() => ClientButtonClick());
     }
 
-    public void HostButtonClick()
+    public async void HostButtonClick()
     {
+        var data = await RelayManager.SetupRelay(10, "production");
+        //await을 쓰는 이유: 인터넷에 연결할 때 시간이 걸리는데 그동안 아예 게임이 멈춰버리면 안되니까
+        
+        NetworkManager.Singleton.GetComponent<UnityTransport>()
+            .SetRelayServerData(data.IPv4Address, data.port, data.allocationIDBytes, data.key, data.connectionData);
+
+        inputField.text = data.joinCode;
+
         NetworkManager.Singleton.StartHost();
     }
 
-    public void ClientButtonClick()
+    public async void ClientButtonClick()
     {
+        var data = await RelayManager.JoinRelay(inputField.text, "production");
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>()
+            .SetRelayServerData(data.IPv4Address, data.port, data.allocationIDBytes, data.key, data.connectionData, data.hostConnectionData);
+        
         NetworkManager.Singleton.StartClient();
     }
 }
